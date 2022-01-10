ed7077fd0cae8a02f32f7157fffe4eec241c4fb0
Kalmyrza <Zholchubekovk@gmail.com>
2022-01-10 18:08:31 +0600

    changes in app

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,23 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useRef } from 'react'
 
 export default function Flashcard({ flashcard }) {
   const [flip, setFlip] = useState(false)
-  const [height, setHeight] = useState('initial')
+  const [height] = useState('initial')
 
   const frontEl = useRef()
   const backEl = useRef()
 
-  function setMaxHeight() {
-    const frontHeight = frontEl.current.getBoundingClientRect().height
-    const backHeight = backEl.current.getBoundingClientRect().height
-    setHeight(Math.max(frontHeight, backHeight, 100))
-  }
-
-  useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options])
-  useEffect(() => {
-    window.addEventListener('resize', setMaxHeight)
-    return () => window.removeEventListener('resize', setMaxHeight)
-  }, [])
 
   return (
     <div
